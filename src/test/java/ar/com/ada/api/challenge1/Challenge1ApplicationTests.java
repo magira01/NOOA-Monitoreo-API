9f95110ce15a5862b7e169cb7dd2da51ae748b9a
magira01 <magira01@hotmail.com>
2021-08-24 19:45:01 -0300

    first commit

---
+++ src/test/java/ar/com/ada/api/challenge1/Challenge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.challenge1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
